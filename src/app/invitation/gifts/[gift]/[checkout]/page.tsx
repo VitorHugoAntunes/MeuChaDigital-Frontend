3dcc738434c629d71d14d67ed02dd3af7522844e
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-09 00:32:46 -0300

    feat: add loading to invitastion gift checkout

--- src/app/invitation/gifts/[gift]/[checkout]/page.tsx
+++ src/app/invitation/gifts/[gift]/[checkout]/page.tsx
@@ -2,7 +2,21 @@
 "use client";
 
 import CheckoutContent from "@/components/CheckoutPage/CheckoutContent";
+import { useGetInvitationGift } from "@/hooks/invitation";
+import InvitationLayout from "@/layouts/InvitationLayout/layout";
+import { useParams } from "next/navigation";
 
 export default function CheckoutPage() {
-  return <CheckoutContent isInvitationPage={true} />;
+  const giftId = useParams().gift as string;
+  const { data: gift, isLoading, error } = useGetInvitationGift(giftId);
+
+  if (isLoading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <InvitationLayout title={gift.list.name || undefined}>
+      <CheckoutContent isInvitationPage={true} />
+    </InvitationLayout>
+  );
 }
