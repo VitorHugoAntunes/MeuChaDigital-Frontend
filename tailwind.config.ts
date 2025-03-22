38ecd6372d2b2d4fdc5c2ca34fec655a7976ba71
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-22 18:23:14 -0300

    style: add custom breakpoints and responsive font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ export default {
     "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '670px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         background: colors.gray[50],
@@ -73,4 +80,5 @@ export default {
     },
   },
   plugins: [],
+
 } satisfies Config;
