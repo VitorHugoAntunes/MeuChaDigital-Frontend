c5a98776140e440953b6c5d9d8b3c6e06ff2f742
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-24 18:23:25 -0300

    feat: add active state to Nav Link

--- src/components/NavLink/index.tsx
+++ src/components/NavLink/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface NavLinkProps {
   href: string;
@@ -6,20 +9,42 @@ interface NavLinkProps {
   variant?: 'primary' | 'secondary';
   icon?: React.ReactNode;
   onClick?: () => void;
+  exact?: boolean;
 }
 
-export default function NavLink({ href, children, variant, icon, onClick }: NavLinkProps) {
+export default function NavLink({
+  href,
+  children,
+  variant = 'primary',
+  icon,
+  onClick,
+  exact = true
+}: NavLinkProps) {
+  const pathname = usePathname();
+  const isActive = exact ? pathname === href : pathname.startsWith(href);
+
+  const baseClasses = 'flex items-center space-x-2 transition-colors duration-300';
+
+  let className = `${baseClasses} `;
+
+  if (variant === 'primary') {
+    className += 'text-text-primary hover:text-primary-light';
+  } else {
+    className += 'text-text-secondary hover:text-primary-light';
+  }
+
+  if (isActive) {
+    className = `${baseClasses} text-primary-light font-semibold border-b-2 border-primary-light hover:text-primary-dark`;
+  }
+
   return (
     <Link
       href={href}
-      className={`flex items-center space-x-2 transition-colors duration-300 ${variant === 'primary'
-        ? 'text-primary-light hover:text-primary-dark'
-        : 'text-text-primary hover:text-primary-light'
-        }`}
+      className={className}
       onClick={onClick}
     >
       <span>{children}</span>
-      <span className="flex items-center justify-center">{icon}</span>
+      {icon && <span className="flex items-center justify-center">{icon}</span>}
     </Link>
   );
 }
