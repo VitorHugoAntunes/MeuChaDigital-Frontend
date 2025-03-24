bbffc3f6df8264ca1800d9d554cceb4e4b2904ed
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-24 18:37:03 -0300

    style: remove a nav link and fix flex box on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -62,7 +62,6 @@ export default function Header() {
 
         <nav className="hidden lg:flex justify-center space-x-8 items-center">
           <div className="flex space-x-8">
-            <div className="h-6 w-24 bg-gray-200 rounded-md animate-pulse"></div>
             <div className="h-6 w-16 bg-gray-200 rounded-md animate-pulse"></div>
             <div className="h-6 w-40 bg-gray-200 rounded-md animate-pulse"></div>
           </div>
@@ -82,72 +81,65 @@ export default function Header() {
 
   return (
     <>
-      <header className="bg-background text-text-primary border-b-2 border-b-gray-200 py-6 px-4 md:px-6 lg:px-8 flex justify-between items-center">
-        <Link href="/" className="group hover:text-primary-dark transition-colors duration-300 w-fit inline-block">
-          <div className="flex items-center space-x-2">
-            <Image
-              src={logo}
-              alt="Meu Chá Digital"
-              className="h-6 w-6 transition-all duration-300 group-hover:brightness-90 lg:h-8 lg:w-8"
-            />
-            <h1 className="text-base font-semibold text-primary-light transition-all duration-300 group-hover:brightness-90 lg:text-2xl">
-              Meu Chá Digital
-            </h1>
-          </div>
-        </Link>
+      <header className="bg-background text-text-primary border-b-2 border-b-gray-200 py-6 px-4 md:px-6 lg:px-8 flex items-center">
+        <div className="flex-1 flex justify-start">
+          <Link href="/" className="group hover:text-primary-dark transition-colors duration-300 w-fit inline-block">
+            <div className="flex items-center space-x-2">
+              <Image
+                src={logo}
+                alt="Meu Chá Digital"
+                className="h-6 w-6 transition-all duration-300 group-hover:brightness-90 lg:h-8 lg:w-8"
+              />
+              <h1 className="text-base font-semibold text-primary-light transition-all duration-300 group-hover:brightness-90 lg:text-2xl">
+                Meu Chá Digital
+              </h1>
+            </div>
+          </Link>
+        </div>
 
-        {isAuthenticated && user ? (
-          <>
-            <nav className="hidden lg:flex justify-center space-x-8 items-center">
-              <ul className="flex space-x-8">
-                <li>
-                  <NavLink href="/">Como funciona?</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/help">Ajuda</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/lists">Minhas listas de presentes</NavLink>
-                </li>
-              </ul>
-            </nav>
+        <nav className="hidden lg:flex flex-1 justify-center items-center">
+          {isAuthenticated && user ? (
+            <ul className="flex space-x-8">
+              <li>
+                <NavLink href="/lists">Minhas listas de presentes</NavLink>
+              </li>
+              <li>
+                <NavLink href="/help">Ajuda</NavLink>
+              </li>
+            </ul>
+          ) : (
+            <ul className="flex space-x-8">
+              <li>
+                <NavLink href="/help">Ajuda</NavLink>
+              </li>
+            </ul>
+          )}
+        </nav>
 
-            <div className="hidden lg:flex items-center space-x-4 justify-end">
-              {user?.name ? (
-                <span className="text-text-primary">Olá, {user.name}</span>
-              ) : (
-                <div className="w-24 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+        <div className="flex-1 flex justify-end items-center space-x-4">
+          {isAuthenticated && user ? (
+            <>
+              {user?.name && (
+                <span className="hidden lg:inline text-text-primary">Olá, {user.name}</span>
               )}
-              <Link href="/profile">
+              <Link className="hidden lg:inline" href="/profile">
                 <Button>Ver perfil</Button>
               </Link>
-            </div>
-          </>
-        ) : (
-          <>
-            <nav className="hidden lg:flex justify-center space-x-8 items-center">
-              <ul className="flex space-x-8">
-                <li>
-                  <NavLink href="/">Como funciona?</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/">Ajuda</NavLink>
-                </li>
-              </ul>
-            </nav>
-            <div className="hidden lg:flex space-x-6 items-center justify-end">
+            </>
+          ) : (
+            <div className="hidden lg:flex space-x-4">
               <Button variant="outlined" onClick={handleLogin}>
                 Entrar
               </Button>
               <Button onClick={handleLogin}>Criar conta</Button>
             </div>
-          </>
-        )}
+          )}
 
-        <div className="lg:hidden flex justify-end">
-          <button onClick={toggleMenu} className="text-text-primary">
-            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-          </button>
+          <div className="lg:hidden flex justify-center items-center">
+            <button onClick={toggleMenu} className="text-text-primary">
+              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+          </div>
         </div>
       </header>
 
@@ -174,16 +166,6 @@ export default function Header() {
 
           <nav className="flex-1 flex flex-col items-center w-full">
             <ul className="text-center w-full">
-              <li className="flex w-full justify-center py-4 border-b-2 border-gray-200 hover:bg-gray-100 transition rounded-md">
-                <NavLink href="/" onClick={closeMenu}>
-                  Como funciona?
-                </NavLink>
-              </li>
-              <li className="flex w-full justify-center py-4 border-b-2 border-gray-200 hover:bg-gray-100 transition rounded-md">
-                <NavLink href="/help" onClick={closeMenu}>
-                  Ajuda
-                </NavLink>
-              </li>
               {isAuthenticated && user && (
                 <li className="flex w-full justify-center py-4 border-b-2 border-gray-200 hover:bg-gray-100 transition rounded-md">
                   <NavLink href="/lists" onClick={closeMenu}>
@@ -191,6 +173,11 @@ export default function Header() {
                   </NavLink>
                 </li>
               )}
+              <li className="flex w-full justify-center py-4 border-b-2 border-gray-200 hover:bg-gray-100 transition rounded-md">
+                <NavLink href="/help" onClick={closeMenu}>
+                  Ajuda
+                </NavLink>
+              </li>
             </ul>
           </nav>
 
