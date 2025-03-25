65a6cdba5e43ce72694bfc281b205aab9f721565
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-25 14:35:18 -0300

    fix: fix next headers

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,18 @@ const nextConfig: NextConfig = {
       }
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
