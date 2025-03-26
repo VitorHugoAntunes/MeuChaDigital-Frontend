87e77878ec0993d31047bc89275f7e835129f83e
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-26 01:26:25 -0300

    fix: fix axios invitation request

--- src/api/invitation.ts
+++ src/api/invitation.ts
@@ -7,21 +7,34 @@ export const getInvitation = async () => {
 
     if (parts.length >= 2 && (parts[1] === 'localhost' || parts[1] === 'meuchadigital')) {
       const subdomain = parts[0];
-
       const apiUrl = `https://${subdomain}.meuchadigital.com/api/v1/invitation`;
-      // const apiUrl = `https://${subdomain}.localhost:8000/api/v1/invitation`;
 
       try {
+        console.log('Tentando chamar URL:', apiUrl); // Log antes da chamada
         const response = await axios.get(apiUrl);
+        console.log('Resposta recebida de:', response.config.url); // Log após sucesso
         return response.data;
       } catch (error) {
-        console.error('Erro ao buscar convite:', error);
+        // Verificação de erro do Axios alternativa
+        if (error.response) {
+          console.error('Erro na resposta:', {
+            url: error.config.url,
+            status: error.response.status,
+            data: error.response.data
+          });
+        } else if (error.request) {
+          console.error('Erro na requisição:', {
+            url: apiUrl,
+            message: 'Nenhuma resposta recebida'
+          });
+        } else {
+          console.error('Erro ao configurar requisição:', error.message);
+        }
         return null;
       }
     }
   }
 };
-
 
 export const getInvitationGift = async (id: string) => {
   if (typeof window !== 'undefined') {
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,29 +4,20 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') {
-    const hostname = window.location.hostname;
-
-    // Check for local development or Vercel deployment
-    if (hostname.includes('localhost') || hostname.endsWith('meuchadigital.com')) {
-      // For Vercel deployments, use HTTPS
-      const protocol = hostname.includes('localhost') ? 'http' : 'https';
-      if (protocol === 'https') {
-        return 'https://api.meuchadigital.com/api/v1';
-      }
-
-      // return 'http://localhost:8000/api/v1';
+    if (!window.location.hostname.includes('localhost')) {
+      console.log('passou pelo localhost');
       return 'https://api.meuchadigital.com/api/v1';
     }
+
+    return 'https://api.meuchadigital.com/api/v1';
   }
 
-  // Fallback to environment variable or default local URL
-  // return process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000/api/v1';
   return process.env.NEXT_PUBLIC_BASE_URL || 'https://api.meuchadigital.com/api/v1';
 };
 
 const api = axios.create({
   baseURL: getBaseUrl(),
-  withCredentials: true, // If you need to send cookies
+  withCredentials: true,
 });
 
 export default api;
