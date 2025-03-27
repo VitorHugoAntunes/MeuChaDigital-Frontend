feedd05c26b1850a5e3deba0e503438ad3d54d48
Vitor Hugo <vitor4ntunes@gmail.com>
2025-03-27 14:53:05 -0300

    fix: fix auth login request

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,9 @@
 import api, { BASE_URL } from '../config/axios';
 
 export const login = async () => {
-  window.open(`${BASE_URL}/auth/google`, '_self');
+  // window.open(`${BASE_URL}/auth/google`, '_self');
+  const response = await api.get('/auth/google', { withCredentials: true });
+  return response.data;
 };
 
 export const logout = async () => {
